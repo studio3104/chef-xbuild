4e7abb452904599f0e07a4ff0d0842366b7bbfe8
studio3104 <studio3104.com@gmail.com>
2013-04-03 13:30:59 +0900

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,3 @@ default['xbuild']['path'] = '/usr/local/bin/xbuild'
 default['perl_build']['bin'] = "#{default['xbuild']['path']}/bin/perl-build"
 default['perl_build']['fatpack_cpanm'] = "#{default['xbuild']['path']}/bin/fatpack_cpanm"
 default['ruby_build']['bin'] = "#{default['xbuild']['path']}/bin/ruby-build"
-
